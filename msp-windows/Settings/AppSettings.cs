df6a330fad2836d9de795fbb9bc186209b00c1fb
okkk2010 <okkk2010@naver.com>
2026-05-28 03:34:47 +0900

    Update AppSettings.cs

--- msp-windows/Settings/AppSettings.cs
+++ msp-windows/Settings/AppSettings.cs
@@ -9,7 +9,7 @@ namespace msp_windows.Settings
         {
             return new AppSettings
             {
-                ServerBaseUrl = "http://localhost:8080",
+                ServerBaseUrl = "https://api.msp-overlay.store:8080",
                 AccessToken = null,
                 LastSelectedOverlayId = null,
                 CacheEnabled = true
